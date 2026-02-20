0479a4cc830f4e59cb78531c48fa18338abb07af
raghavendra3107 <raghavendrasiva.npt@gmail.com>
2026-02-20 13:56:38 +0530

    Fixed auth API routes

--- client/src/redux/features/auth/authActions.js
+++ client/src/redux/features/auth/authActions.js
@@ -6,7 +6,7 @@ export const userLogin = createAsyncThunk(
   "auth/login",
   async ({ role, email, password }, { rejectWithValue }) => {
     try {
-      const { data } = await API.post("/auth/login", { role, email, password });
+      const { data } = await API.post("/api/v1/auth/login", { role, email, password });
       //store token
       if (data.success) {
         alert(data.message);
@@ -42,7 +42,7 @@ export const userRegister = createAsyncThunk(
     { rejectWithValue }
   ) => {
     try {
-      const { data } = await API.post("/auth/register", {
+      const { data } = await API.post("/api/v1/auth/register", {
         name,
         role,
         email,
@@ -72,9 +72,9 @@ export const userRegister = createAsyncThunk(
 //current user
 export const getCurrentUser = createAsyncThunk(
   "auth/getCurrentUser",
-  async ({ rejectWithValue }) => {
+  async (_, { rejectWithValue }) => {
     try {
-      const res = await API.get("/auth/current-user");
+      const res = await API.get("/api/v1/auth/current-user");
       if (res.data) {
         return res?.data;
       }
